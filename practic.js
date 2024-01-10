7fe5ce31d483e6273534760c3d41068ccdd4b4f2
David Pron <92371541+DavidPron@users.noreply.github.com>
2024-01-10 22:17:24 +0200

    Add files via upload

---
+++ practic.js
@@ -0,0 +1,19 @@
+"use sctrict";
+
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log("No");
+        break;
+    case 100:
+        console.log("Fack no");
+        break;
+    case 50:
+        console.log("GId dam right");
+        break;
+    default:
+        console.log("Not today");
+        break;
+}
